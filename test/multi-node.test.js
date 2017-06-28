88175fae4ac7304308f1527311b74ab5a93d0666
Alex Wilson <alex.wilson@joyent.com>
2017-06-27 17:58:20 -0700

    joyent/node-zkstream#17 downed zk server coming back causes crash

--- test/multi-node.test.js
+++ test/multi-node.test.js
@@ -218,6 +218,125 @@ mod_tape.test('cross-server data watch', function (t) {
 	}
 });
 
+mod_tape.test('ephemeral node failover', function (t) {
+	var closed = 0, connected = 0;
+	var created = 0, deleted = 0;
+
+	var zkc1 = new mod_zkc.Client({
+		log: log,
+		servers: Object.keys(zks).map(function (k) {
+			return ({
+			    address: '127.0.0.1',
+			    port: zks[k].clientPort
+			});
+		}),
+		sessionTimeout: 10000
+	});
+
+	/* Testing hack: force cueball to connect to backends in order */
+	setImmediate(function () {
+		zkc1.zc_set.cs_keys.sort();
+		var k0 = zkc1.zc_set.cs_keys[0];
+		var zkPort = zkc1.zc_set.cs_backends[k0].port;
+		t.strictEqual(zkPort, zks['1'].clientPort);
+		t.notStrictEqual(zkPort, zks['3'].clientPort);
+	});
+
+	var zkc2 = new mod_zkc.Client({
+		log: log,
+		address: '127.0.0.1',
+		port: zks['3'].clientPort
+	});
+
+	zkc1.on('close', function () {
+		if (++closed >= 2)
+			t.end();
+	});
+
+	zkc2.on('close', function () {
+		if (++closed >= 2)
+			t.end();
+	});
+
+	zkc1.on('connect', function () {
+		if (++connected == 2) {
+			setup();
+		}
+	});
+
+	zkc2.on('connect', function () {
+		if (++connected == 2) {
+			setup();
+		}
+	});
+
+	function setup() {
+		var data = new Buffer('hello world');
+		var opts = { flags: ['EPHEMERAL'] };
+		zkc2.watcher('/foo.ephem').on('created', function () {
+			++created;
+			zkc2.watcher('/foo.ephem').on('deleted', function () {
+				++deleted;
+			});
+		});
+		zkc1.create('/foo.ephem', data, opts, function (err, path) {
+			t.error(err);
+			t.strictEqual(path, '/foo.ephem');
+			zkc1.sync('/foo.ephem', function (err2) {
+				t.error(err2);
+				zkc2.sync('/foo.ephem', function (err3) {
+					t.error(err3);
+					t.ok(created > 0, 'saw created');
+					t.ok(deleted === 0, 'saw no deleted');
+					kill();
+				});
+			});
+		});
+	}
+
+	function kill() {
+		zk1.on('stateChanged', function (st) {
+			if (st === 'stopped') {
+				setTimeout(check, 11000);
+			}
+		});
+		zk1.stop();
+	}
+
+	function check() {
+		t.ok(created > 0, 'saw created');
+		t.ok(deleted === 0, 'saw no deleted');
+		zkc2.get('/foo.ephem', function (err, data) {
+			t.error(err);
+			t.strictEqual(data.toString(), 'hello world');
+			unkill();
+		});
+	}
+
+	function unkill() {
+		zk1 = new mod_zk.ZKServer({
+			servers: zks,
+			serverId: '1'
+		});
+		zk1.on('stateChanged', function (st) {
+			if (st === 'running') {
+				setTimeout(checkAgain, 10000);
+			}
+		});
+	}
+
+	function checkAgain() {
+		t.ok(created > 0, 'saw created');
+		t.ok(deleted === 0, 'saw no deleted');
+		zkc1.get('/foo.ephem', function (err, data) {
+			t.error(err);
+			t.strictEqual(data.toString(), 'hello world');
+			zkc1.close();
+			zkc2.close();
+		});
+	}
+});
+
 mod_tape.test('stop zk servers', function (t) {
 	var stopped = 0;
 	zk1.on('stateChanged', function (st) {
